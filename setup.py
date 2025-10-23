442fcbcbc07d7bbcd2c9928df46630edbcf71ab0
rkpcode <rkpfarmx@gmail.com>
2025-10-23 10:36:16 +0530

    5th commit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def get_requirements(file_path: str) -> List[str]:
     """Reads the requirements from a file and returns them as a list."""
     requirements = []
     with open(file_path) as file:
-        requirements = [req.strip() for req in file.readlines()]
+        requirements = [req.replace('\n',"") for req in file.readlines()]
         requirements = [req for req in requirements if req != '-e .']
     return requirements
 
