fa22a0a1ecce6a13afb50c23f6fed006052956d2
rkpcode <rkpfarmx@gmail.com>
2025-10-25 17:31:41 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
-
+from src.mlproject.components.data_ingestion import DataIngestion
+import sys
 
 if __name__ == "__main__":
     logging.info("Starting the ML Project Application")
@@ -8,5 +9,10 @@ if __name__ == "__main__":
     logging.info("ML Project Application Finished")
 
     try:
-        a=1/0
-    except Exception.info("Custom Exception")
+        data_ingestion = DataIngestion()
+        # initiate_data_ingestion expects a file_path parameter in the signature; pass an empty string
+        data_ingestion.initiate_data_ingestion("")
+
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e, sys)
---
+++ src/mlproject/utils.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from dotenv import load_dotenv
+import pymysql
+
+
+
+load_dotenv()
+
+host = os.getenv('host')
+user = os.getenv('user')
+password = os.getenv('password')
+db = os.getenv('db')
+
+def read_sql_data():
+    logging.info("Reading SQL database")
+    try:
+        mydb = pymysql.connect(
+            host = host,
+            user = user,
+            password = password,
+            db = db
+        )
+        logging.info("Connection Established",mydb)
+        df = pd.read_sql_query('SELECT * FROM survey_data', mydb)
+        print(df.head())
+
+        return df
+        
+    except Exception as ex:
+        raise CustomException(ex)
+
+
+
