50bc6d514f622b1bc064a93fee940473703640d3
rkpcode <rkpfarmx@gmail.com>
2025-11-04 10:07:35 +0530

    model training

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_transformation import DataTransformation
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 import sys
 
 if __name__ == "__main__":
@@ -15,9 +16,25 @@ if __name__ == "__main__":
         # an empty string is acceptable as a placeholder.
         train_data_path, test_data_path = data_ingestion.initiate_data_ingestion("")
 
-        # Run data transformation
+        # Run data transformation and get transformed arrays
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transormation(train_data_path, test_data_path)
+        train_array, test_array, _ = data_transformation.initiate_data_transormation(train_data_path, test_data_path)
+        
+        # Model Training
+        model_trainer = ModelTrainer()
+        trained_model_score = model_trainer.initiate_model_trainer(train_array, test_array)
+        logging.info(f"Model training completed. Score: {trained_model_score}")
+
+
+
+
+
+
+
+
+
+
+
 
         logging.info("ML Project Application Finished")
 
