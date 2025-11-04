6bd0d98d461ff592ebe10a2d15fee884f4308ee0
rkpcode <rkpfarmx@gmail.com>
2025-11-04 11:08:31 +0530

    ML flow integrated

--- src/mlproject/components/model_trainer.py
+++ src/mlproject/components/model_trainer.py
@@ -2,8 +2,8 @@ import os
 import sys
 from dataclasses import dataclass
 from urllib.parse import urlparse
-#import mlflow
-#import mlflow.sklearn
+import mlflow
+import mlflow.sklearn
 import numpy as np
 from sklearn.metrics import mean_squared_error,mean_absolute_error
 from catboost import CatBoostRegressor
@@ -21,6 +21,8 @@ from xgboost import XGBRegressor
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 from src.mlproject.utils import save_object,evaluate_models
+import dagshub
+
 
 
 @dataclass
@@ -119,9 +121,12 @@ class ModelTrainer:
                     actual_model = actual_model + model
 
             best_params = params[actual_model]
-            '''
+            
+            #dagshub
+            dagshub.init(repo_owner='rkpcode', repo_name='ML_projects', mlflow=True)
+            
 
-            mlflow.set_registry_uri("https://dagshub.com/krishnaik06/mlprojecthindi.mlflow")
+            mlflow.set_registry_uri("https://dagshub.com/rkpcode/ML_projects.mlflow")
             tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
             # mlflow
@@ -133,24 +138,16 @@ class ModelTrainer:
                 (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
 
                 mlflow.log_params(best_params)
-
                 mlflow.log_metric("rmse", rmse)
                 mlflow.log_metric("r2", r2)
                 mlflow.log_metric("mae", mae)
 
-
-                # Model registry does not work with file store
-                if tracking_url_type_store != "file":
-
-                    # Register the model
-                    # There are other ways to use the Model Registry, which depends on the use case,
-                    # please refer to the doc for more information:
-                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
-                else:
-                    mlflow.sklearn.log_model(best_model, "model")
-
-                '''
+                # Save the best model locally instead of logging to MLflow
+                import pickle
+                with open("artifacts/best_model.pkl", "wb") as f:
+                    pickle.dump(best_model, f)
+                # Removed all mlflow.sklearn.log_model calls
+                
 
 
             if best_model_score<0.6:
